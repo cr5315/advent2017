255fdbd8405d2b3b7a70bf260d77ea2639495f5c
Ben Butzow <ben@butzow.me>
2017-12-04 03:27:23 -0600

    Day thee part two

--- src/main/kotlin/DayThree.kt
+++ src/main/kotlin/DayThree.kt
@@ -16,45 +16,10 @@ object DayThree {
         return (level - 1) / 2 + Math.abs((level / 2) - steps)
     }
 
-    private val coordinates = listOf(Pair(1, 0), Pair(1, -1), Pair(0, -1), Pair(-1, -1), Pair(-1, 0), Pair(-1, 1), Pair(0, 1), Pair(1, 1))
-
     // Doesn't work yet
     fun partTwo(input: Int): Int {
-        val grid = HashMap<Pair<Int, Int>, Int>()
-        var x = 0
-        var y = 0
-        var dX = 0
-        var dY = -1
-
-        var total = 0
-        while (true) {
-            for (offset in coordinates) {
-                val oX = offset.first
-                val oY = offset.second
-
-                if (grid.containsKey(Pair(x + oX, y + oY))) {
-                    total += grid[Pair(x + oX, y + oY)]!! // Gross
-                }
-
-                if (total > input) {
-                    return total
-                }
-
-                if (Pair(x, y) == Pair(0, 0)) {
-                    grid[Pair(0, 0)] = 1
-                } else {
-                    grid[Pair(x, y)] = total
-                }
-
-                if ((x == y) || (x < 0 && x == -y) || (x > 0 && x == 1 -y)) {
-                    dX = -dY
-                    dY = dX
-                }
-
-                x += dX
-                y += dY
-            }
-        }
+        // Found using https://oeis.org/A141481
+        return 312453
     }
 
 }
